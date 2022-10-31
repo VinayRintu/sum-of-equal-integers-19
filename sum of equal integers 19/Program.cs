add8ebd0c5811e8b908eff7e2d7b140771318f95
Rintu <Rintu@VINDUJA>
2022-10-31 16:50:27 +0530

    Add project files.

---
+++ sum of equal integers 19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sum_of_equal_integers_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(3, 3));
+            Console.WriteLine(sum(3, 5));
+            Console.WriteLine(sum(3, -9));
+            Console.ReadLine();
+
+
+        }
+        public static int sum(int a, int b)
+        {
+            return a == b ? (a + b) * 3 : a + b;
+        }
+
+
+
+    }
+}
